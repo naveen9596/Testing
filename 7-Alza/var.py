1d10414ffd0719a3d598c26a19c6a0e12765cd5b
Tomas Pekarovic <tomas.pekarovic@artinsolutions.com>
2021-11-25 14:12:41 +0100

    ADD: Variable files

--- 7-Alza/var.py
+++ 7-Alza/var.py
@@ -1,4 +1,17 @@
-computer_and_laptops = "xpath://a[@class='l0-catLink'][text()='Computers and Laptops']"
-laptops = "xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Laptops']"
-# ${home_and_office}    xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Home & Office']
-# ${affordable}    xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Affordable']
+en = {
+    "computer_and_laptops": "xpath://a[@class='l0-catLink'][text()='Computers and Laptops']",
+    "laptops": "xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Laptops']"
+}
+sk = {
+    "computer_and_laptops": "xpath://a[@class='l0-catLink'][text()='Počítače a notebooky']",
+    "laptops": "xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Počítače']"
+}
+
+
+def get_variables(language):
+    if language == "sk":
+        return sk
+    return en
+
+
+#  robot -V var.py:sk cart.robot
--- 7-Alza/var_sk.py
+++ 7-Alza/var_sk.py
@@ -1,4 +1,4 @@
-computer_and_laptops= "xpath://a[@class='l0-catLink'][text()='Computers and Laptops']"
-laptops="xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Laptops']"
+computer_and_laptops = "xpath://a[@class='l0-catLink'][text()='Počítače a notebooky']"
+laptops = "xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Počítače']"
 # ${home_and_office}    xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Home & Office']
 # ${affordable}    xpath://a[contains(@class, 'subC') and not(contains(@class, 'subCat'))]//span[text()='Affordable']
